28cbacef40fc16a2bc29c1efe1e3fcf6690467c5
Coding rev <fernando.lobo.prez@gmail.com>
2025-06-22 00:11:03 -0300

    03-Wroking with +layouts
    03-Wroking with +layouts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    let { children } = $props()
+</script>
+<div class="container">
+    <header>
+        My app header
+    </header>
+    <hr/>
+    <div>
+        {@render children()}
+    </div>
+</div>
---
+++ src/routes/users/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+    let { children } = $props()
+</script>
+<h3>I am the USERS layout</h3>
+{@render children()}
