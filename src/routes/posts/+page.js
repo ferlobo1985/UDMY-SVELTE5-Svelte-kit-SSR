7f85267ba6f5e750bd2507bec28fce75f41e291d
Coding rev <fernando.lobo.prez@gmail.com>
2025-06-25 03:28:06 -0300

    20- Pre-Render part one
    20- Pre-Render part one

--- src/routes/posts/+page.js
+++ src/routes/posts/+page.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { error, redirect } from '@sveltejs/kit'
 
+export const prerender = true;
+
 export async function load(){
     try{
         const res = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
